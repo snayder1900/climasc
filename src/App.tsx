3c9cd168fd945f16d124479a198c0607cdd71442
snider <snaydergp@hotmail.com>
2025-03-06 12:45:22 -0500

    Proyecto terminado

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import styles from "./App.module.css";
+import { Alert } from "./components/Alert/Alert";
+import { Form } from "./components/Form/Form";
+import { Spinner } from "./components/Spinner/Spinner";
+import { WeatherDetail } from "./components/WeatherDetail/WeatherDetail";
+import useWeather from "./hooks/useWeather";
+
+function App() {
+  const { weather, loading, fetchWeather, hasWeatherData, notFound } =
+    useWeather();
+
+  return (
+    <>
+      <h1 className={styles.title}>Buscador de clima</h1>
+      <div className={styles.container}>
+        <Form fetchWeather={fetchWeather} />
+        {loading && <Spinner />}
+        {hasWeatherData && <WeatherDetail weather={weather} />}
+        {notFound && <Alert>Ciudad no encontrada</Alert>}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Alert/Alert.tsx
@@ -0,0 +1,6 @@
+import { ReactNode } from "react";
+import styles from "./Alert.module.css";
+
+export const Alert = ({ children }: { children: ReactNode }) => {
+  return <div className={styles.alert}>{children}</div>;
+};
